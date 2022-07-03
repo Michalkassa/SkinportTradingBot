9bbced15c862c9f5da43206391b983252ee26571
Michal Kassa <84039535+Michalkassa@users.noreply.github.com>
2022-07-04 00:40:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import base64
 from operator import index
+from types import NoneType
 import requests
 import pprint 
 import sec
@@ -21,30 +22,57 @@ item_data = requests.get("https://api.skinport.com/v1/items?app_id=730¤cy=EUR&t
 }).json()
 
 #get sales
-sales_data = requests.get("https://api.skinport.com/v1/sales/history", params={
+def RequestSale(ItemNameSale):
+    return requests.get("https://api.skinport.com/v1/sales/history", params={
     "app_id": 730,
     "currency": "EUR",
-    "market_hash_name": "Glove Case Key,★ Karambit | Slaughter (Minimal Wear)"
-}).json()
+    "market_hash_name": f"{ItemNameSale}"
+    }).json()
 
-def ApiData(itemName):
-    for i in item_data:
-        if(i['market_hash_name'] == str(itemName)):
-            print("------------------------")
-            print (i['market_hash_name'])
-            print("\n")
-            print (f"Max Price: {i['max_price']}")
-            print (f"Mean Price: {i['mean_price']}")
-            print (f"Min Price: {i['min_price']}")
-            break
+sellprice = 0
+minimumprice = 0
 
-    for i in sales_data:
-        if(i['market_hash_name'] == str(itemName)):
-            print(pp.pprint(i['last_30_days']))
-            print(pp.pprint(i['last_7_days']))
-            print('................................\n')
-            print(i['item_page'])
-            print('\n..............................\n')
-            break
+# def ApiData():
+#     minimumprice = 0 
+#     sellprice = 0
+#     for item in item_data:
+#         minimumprice = item['min_price']
+#         for sale in sales_data:
+#             if sale["market_hash_name"] == item["market_hash_name"]:
+#                 sellprice = int(sale['last_30_days']['max']) * 0.88
+#         return [item['market_hash_name'],sellprice - minimumprice]
 
+
+#         print("\n")
+#         print (f"Max Price: {i['max_price']}")
+#         print (f"Mean Price: {i['mean_price']}")
+#         print (f"Min Price: {i['min_price']}")
+
+    # for i in sales_data:
+    #     sellprice = i['last_30_days']['max'] * 0.88
+#         print(pp.pprint(int(i['last_30_days']['max'])))
+#         print(pp.pprint(i['last_7_days']))
+#         print('................................\n')
+#         print(i['item_page'])
+#         print('\n..............................\n')
+#         print(sellprice - minimumprice)
+        
+
+indx = 0
+while indx <= 2:
+    minimumprice = 0
+    sellprice = 0
+    for item in item_data:
+        minimumprice = 0
+        #type checking for non in the skinport api as prices can be none
+        if type(RequestSale(item["market_hash_name"])[0]["last_7_days"]["min"]) != NoneType:
+            minimumprice = item["min_price"]
+        if type(RequestSale(item["market_hash_name"])[0]["last_7_days"]["min"]) != NoneType:
+            sellprice = RequestSale(item["market_hash_name"])[0]["last_7_days"]["min"]
+
+        #display data by printing
+        print(item['market_hash_name']) 
+        profit = float(sellprice * 0.88 ) - minimumprice
+        print(profit)
+        print(sellprice)
 
--- sec.py
+++ sec.py
@@ -1,5 +1,5 @@
 #client id found in the bottom of the settings on the skinport website
-Id = ""
+Id = "74333c5c97ec47d88e00018311ad33d9"
 
 #client secret found in the bottom of the settings (KEEP THIS ONE SECRET AT ALL COSTS)
-KEY = ""
+KEY = "iZBFSLc/Z8VoMPPlBtG9vYRfX+rVTOZw5HAODRXLb/nKzI7OU50bdOq4/nNkaXhOvso+k2F7+9RbDWJZL2qDcg=="
